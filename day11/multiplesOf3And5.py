2bf46e56eb9767e6c62d1440d4092c560d3fe861
Gerardo Garzón Villar <gerardogv20@gmail.com>
2019-06-28 00:35:43 -0500

    Create multiplesOf3And5.py

---
+++ day11/multiplesOf3And5.py
@@ -0,0 +1,20 @@
+import math
+
+def getSumOfMultiples(number, divisor):
+  """
+  Returns sum of multiples of divisor below number
+  Example:
+  number = 20, divisor = 3
+  result = 63
+  
+  Sum of multiples is 3+6+9+12+15+18
+  """
+  
+	largestMultiple = (number // divisor) * divisor if number % divisor != 0 else number - divisor
+	sumOfMultiples = ((largestMultiple + divisor) * 10 // 2) * ((number - 1) // divisor) // 10
+	return sumOfMultiples
+
+number = int(input())
+
+# Code was used specifically for a Project Euler problem, so this stays like this
+print(getSumOfMultiples(number, 3) + getSumOfMultiples(number, 5) - getSumOfMultiples(number, 15))
