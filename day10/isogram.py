5b0bdc6c86a8de7c18f124cdc66cad63430166d2
Gerardo Garzón Villar <gerardogv20@gmail.com>
2019-07-17 21:24:29 -0500

    Refactored solution

--- day10/isogram.py
+++ day10/isogram.py
@@ -1,14 +1,9 @@
-import string as str
-
 def is_isogram(string):
-
-	if len(string) < 1:
-		return True
 
 	string = string.lower()
 
 	for x in string:
-		if x in str.ascii_lowercase:
+		if x.isalpha():
 			if string.count(x) > 1:
 				return False
 
